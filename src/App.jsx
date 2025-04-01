c49417c0c5a1c2fa6ff30e6f46e22a81dc0a65ae
Guga2111 <luisgosampaio@gmail.com>
2025-04-01 09:57:05 -0300

    feat: ridirect login button implemented to "/login" and the main page to "/" using Router

--- src/App.jsx
+++ src/App.jsx
@@ -5,26 +5,37 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import Descritivo from './Descritivo';
-
+import RedirecionarLogin from './Redirecionarlogin';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Cabecalho/>
-      <Banner/>
-      <Descritivo
-        id = "quem-somos"
-        title = "Quem somos"
-        background = '#D0D7DC'
-        text = 'A COOELPE é uma empresa de Energia Renovável que produz a sua própria energia por meio de painéis solares modernos montados em uma grande estrutura. Isso permite que os nossos clientes tenham custo da sua energia reduzido em até 20% (vinte por cento), gerando assim uma redução na conta de energia da Celpe, sem que haja a exigência de um investimento, telhado ou área.'
-      />
-      <Descritivo
-        id = "nossos-objetivos"
-        title = "Nossos objetivos"
-        background = '#E7E0E0'
-        text = 'Na COOELPE, acreditamos que a inovação e a sustentabilidade devem andar juntas. Nosso objetivo é transformar a forma como você consome energia, oferecendo soluções solares eficientes e acessíveis que reduzem significativamente os custos na sua conta de luz. Com tecnologia de ponta e um compromisso com a excelência, ajudamos residências e empresas a gerar sua própria energia limpa, garantindo economia e um futuro mais sustentável. Invista no sol, economize mais e faça parte da revolução energética com a gente!'
-      />
-    </div>
+    <Router>
+      <div className="App">
+        <RedirecionarLogin /> 
+        <Cabecalho />
+        <Routes>
+          <Route path="/" element={
+            <>
+              <Banner />
+              <Descritivo
+                id="quem-somos"
+                title="Quem somos"
+                background="#D0D7DC"
+                text="A COOELPE é uma empresa de Energia Renovável que produz a sua própria energia por meio de painéis solares modernos montados em uma grande estrutura. Isso permite que os nossos clientes tenham custo da sua energia reduzido em até 20% (vinte por cento), gerando assim uma redução na conta de energia da Celpe, sem que haja a exigência de um investimento, telhado ou área."
+              />
+              <Descritivo
+                id="nossos-objetivos"
+                title="Nossos objetivos"
+                background="#E7E0E0"
+                text="Na COOELPE, acreditamos que a inovação e a sustentabilidade devem andar juntas. Nosso objetivo é transformar a forma como você consome energia, oferecendo soluções solares eficientes e acessíveis que reduzem significativamente os custos na sua conta de luz. Com tecnologia de ponta e um compromisso com a excelência, ajudamos residências e empresas a gerar sua própria energia limpa, garantindo economia e um futuro mais sustentável. Invista no sol, economize mais e faça parte da revolução energética com a gente!"
+              />
+            </>
+          } />
+          <Route path="/login"  /> {/* adicionar aqui o componente login que posteriormente vai ser criado*/}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
